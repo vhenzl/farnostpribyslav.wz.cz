6032e53798b19230ae6cfc9d577164b3643f5f6b
Vašek Henzl <vasek.henzl@gmail.com>
2025-08-28 16:33:47 +1200

    Add content for "o webu" page

--- src/app/o-webu/page.tsx
+++ src/app/o-webu/page.tsx
@@ -1,3 +1,5 @@
+import PageTitle from '@/components/page-title';
+import Prose from '@/components/prose';
 import { toAbsoluteUrl } from '@/lib/site';
 import type { Metadata } from 'next';
 import type { JSX } from 'react';
@@ -17,12 +19,76 @@ export const metadata: Metadata = {
 export default function AboutPage(): JSX.Element {
   return (
     <article>
-      <h1>O webu</h1>
-      <p>
-        <span>Archivní projekt zachovává vybrané části původního webu farnosti Přibyslav.</span>
-        <br />
-        <span>Cílem je čitelnost, dostupnost a stálost odkazů bez potřeby serverového runtime.</span>
-      </p>
+      <PageTitle className="mb-8 mt-4">O webu</PageTitle>
+      <Prose>
+        <p>
+          Tyto stránky byly
+          {' '}
+          <strong>
+            původním oficiálním webem
+            {' '}
+            <em>Římskokatolické farnosti Přibyslav</em>
+            {' '}
+            v letech 2001 až 2008
+          </strong>
+          .
+        </p>
+        <p>
+          První verzi vytvořil
+          {' '}
+          <em>Libor Jaroš</em>
+          {' '}
+          počátkem roku 2001 jako soubor statických HTML stránek.
+          Koncem roku 2002 převzal technickou část
+          {' '}
+          <em>Vašek Henzl</em>
+          {' '}
+          a předělal web na dynamický,
+          což umožnilo snadněji aktualizovat obsah. O dva roky později Vašek celý web přepracoval
+          a v únoru 2005 spustil novou verzi s vylepšeným redakčním systémem, která sloužila až do roku 2008.
+        </p>
+        <p>
+          Po celou dobu byli Libor a Vašek hlavními správci a tvůrci obsahu.
+          Příležitostně se ale podíleli i další farníci, jmenovitě Stanislav Močuba a Tomáš Lehrl.
+        </p>
+        <p>
+          Koncem roku 2007 pastorační rada
+          {' '}
+          <a href="/zpravy/2007-26">rozhodla</a>
+          {' '}
+          o vytvoření nových oficiálních stránek farnosti
+          a v létě 2008 spustil Tomáš Miškovský z
+          {' '}
+          <i>Fenomen multimedia</i>
+          {' '}
+          nový web na
+          {' '}
+          <a href="https://www.farnostpribyslav.cz">www.farnostpribyslav.cz</a>
+          .
+          Tím se uzavřela aktivní kapitola farních stránek na
+          {' '}
+          <a href="http://farnostpribyslav.wz.cz">farnostpribyslav.wz.cz</a>
+          .
+          Starý web ale běžel dál a sloužil jako archiv zpráv.
+        </p>
+        <p>
+          Osud redakčního systému z roku 2005 se završil o dvacet let později.
+          V dubnu 2025
+          {' '}
+          <i>Web zdarma</i>
+          {' '}
+          <a href="https://www.webzdarma.cz/novinky/detail/migrace-dokoncena-209">ukončil</a>
+          {' '}
+          podporu zastaralé verze PHP, na jejíž vlastnosti systém spoléhal,
+          a celý web se svým historickým obsahem se stal nedostupným.
+        </p>
+        <p>
+          V srpnu 2025 se Vašek rozhodl web zachránit a vytvořil tento
+          {' '}
+          <strong>archiv s vybraným obsahem starého webu</strong>
+          .
+        </p>
+      </Prose>
     </article>
   );
 }
