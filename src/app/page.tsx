1674f5cfcec4114e74825f400fc7e548f2a22a5a
Vašek Henzl <vasek.henzl@gmail.com>
2025-08-29 19:27:50 +1200

    Fix styling of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export const metadata: Metadata = {
 export default async function Page(): Promise<JSX.Element> {
   const years: number[] = await getYearsSummary();
   return (
-    <section className="flex items-center justify-center min-h-[60vh] flex-col text-stone-800">
-      <PageTitle className="sm:text-5xl mb-4">Farnost Přibyslav</PageTitle>
+    <section className="flex items-center justify-center min-h-[60vh] py-8 flex-col text-stone-800">
+      <PageTitle className="sm:text-5xl text-center mb-4">Farnost Přibyslav</PageTitle>
       <p className="text-amber-600 font-semibold text-xl sm:text-2xl text-center mb-12">
         Archiv webových stránek farnosti Přibyslav z let 2001 až 2008.
       </p>
@@ -43,7 +43,7 @@ export default async function Page(): Promise<JSX.Element> {
         .
       </p>
 
-      <p className="mb-2">Rychlé odkazy na zprávy z jednotlivých let:</p>
+      <p className="mb-2 text-center">Rychlé odkazy na zprávy z jednotlivých let:</p>
       <div className="mb-12 flex flex-wrap justify-center gap-3">
         {years.map(y => (
           <BrandLink key={y} className="font-semibold" href={`/zpravy#rok-${y}`}>
@@ -51,7 +51,7 @@ export default async function Page(): Promise<JSX.Element> {
           </BrandLink>
         ))}
       </div>
-      <p className="text-center font">
+      <p className="text-center">
         Současné webové stránky farnosti Přibyslav naleznete na
         {' '}
         <BrandLink className="font-bold" href="https://www.farnostpribyslav.cz">www.farnostpribyslav.cz</BrandLink>
